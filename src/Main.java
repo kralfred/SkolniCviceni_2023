40ea5d1a6b9852b515603b59faeebae125dadb98
Withers Alfred <witheal1@uhk.cz>
2023-02-14 10:25:32 +0100

    pridane pole

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
-    
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("ahoj");
     }
+    public static void ahoj(druha[]){
+      prvni [1] = 8;
+      druha [2] = 10;
+    }
+
+
+
 }
